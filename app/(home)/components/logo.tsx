67b6bc8072d96c809708b6aba3fa1fe77cadc21d
AbdulazizRus <coderdim@gmail.com>
2024-08-02 22:19:55 +0500

    shadcn

--- app/(home)/components/logo.tsx
+++ app/(home)/components/logo.tsx
@@ -1,3 +1,16 @@
+import Image from 'next/image'
+
 export const Logo = () => {
-  return <div>Logo</div>
+  return (
+    <div className="flex items-center gap-x-2">
+      <Image
+        src={'/logo.svg'}
+        alt="logo"
+        width={50}
+        height={50}
+        className="object-cover"
+      />
+      <p className='font-semibold text-xl'>Notion</p>
+    </div>
+  )
 }
--- app/(home)/components/navbar.tsx
+++ app/(home)/components/navbar.tsx
@@ -4,6 +4,9 @@ export const Navbar = () => {
   return (
     <div className="z-50 bg-background fixed top-0 flex items-center w-full p-6">
       <Logo />
+      <div className="flex items-center gap-x-2">
+        
+      </div>
     </div>
   )
 }
