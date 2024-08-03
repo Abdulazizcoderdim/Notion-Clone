d351194f30bd1a2f6baca1a034891e9589f01207
AbdulazizRus <coderdim@gmail.com>
2024-08-03 09:01:25 +0500

    changes

--- app/(home)/components/index.ts
+++ app/(home)/components/index.ts
@@ -1,5 +1,6 @@
 import { Heroes } from './heroes'
 import { Logo } from './logo'
 import { Navbar } from './navbar'
+import { Clients } from './clients'
 
-export { Heroes, Logo, Navbar }
+export { Heroes, Logo, Navbar, Clients }
