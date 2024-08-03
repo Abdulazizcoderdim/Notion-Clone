e4095116b11e4ee80457e6148a193b57cb38a94b
AbdulazizRus <coderdim@gmail.com>
2024-08-03 23:55:01 +0500

    landing page

--- app/(home)/components/navbar.tsx
+++ app/(home)/components/navbar.tsx
@@ -1,10 +1,21 @@
+'use client'
+
 import { ModeToggle } from '@/components/shared/ModeToggle'
 import { Button } from '@/components/ui/button'
+import { useScrolled } from '@/hooks/use-scrolled'
+import { cn } from '@/lib/utils'
 import { Logo } from '.'
 
 export const Navbar = () => {
+  const scrolled = useScrolled()
+
   return (
-    <div className="z-50 justify-between bg-background fixed top-0 flex items-center w-full p-6">
+    <div
+      className={cn(
+        'z-50 justify-between bg-background fixed top-0 flex items-center w-full p-6',
+        scrolled && 'border-b shadow-sm'
+      )}
+    >
       <Logo />
       <div className="flex items-center gap-x-2">
         <Button size={'sm'} variant={'ghost'}>
