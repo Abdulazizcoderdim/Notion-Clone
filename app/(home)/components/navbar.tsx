08e42d90e8791fa1c460cd46ac8d51a7f1bb8d5e
AbdulazizRus <coderdim@gmail.com>
2024-08-04 13:17:06 +0500

    chnages

--- app/(home)/components/navbar.tsx
+++ app/(home)/components/navbar.tsx
@@ -4,9 +4,13 @@ import { ModeToggle } from '@/components/shared/ModeToggle'
 import { Button } from '@/components/ui/button'
 import { useScrolled } from '@/hooks/use-scrolled'
 import { cn } from '@/lib/utils'
+import { SignInButton, UserButton } from '@clerk/clerk-react'
+import { useConvexAuth } from 'convex/react'
+import Link from 'next/link'
 import { Logo } from '.'
 
 export const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth()
   const scrolled = useScrolled()
 
   return (
@@ -18,10 +22,27 @@ export const Navbar = () => {
     >
       <Logo />
       <div className="flex items-center gap-x-2">
-        <Button size={'sm'} variant={'ghost'}>
-          Log In
-        </Button>
-        <Button size={'sm'}>Get Notion Free</Button>
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button size={'sm'} variant={'ghost'}>
+                Log In
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size={'sm'}>Get Notion Free</Button>
+            </SignInButton>
+          </>
+        )}
+
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button asChild variant={'ghost'} size={'sm'}>
+              <Link href={'/documents'}>Enter Notion</Link>
+            </Button>
+            <UserButton afterSignOutUrl='/'/>
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
